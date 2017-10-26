c76db43fae64965fd88759d2cd9a0106ed86c6cd
Roper <griffinroboticsaz@gmail.com>
2017-10-26 15:19:15 -0700

    Added new file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -11,6 +12,7 @@ import com.qualcomm.robotcore.util.Range;
  * Created by Griffins on 9/11/2017.
  */
 @TeleOp(name = "TestOpMode", group = "TeleOp")
+//@Disabled
 public class TestOpMode extends OpMode{
 
     public DcMotor left;
